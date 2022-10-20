57d6d9625b25a2bf8532820f5280e0675d17339b
Zan Zirbel <zan.zirbel@gmail.com>
2022-10-20 11:55:15 -0700

    create getPlayerChoice function to seperate player choice prompt and casing from playRound function

--- scripts.js
+++ scripts.js
@@ -10,34 +10,47 @@ function getComputerChoice() {
     return choices[Math.ceil(Math.random() * 3)];
 }
 
+/* prompt player selection and change to first letter casing */
+function getPlayerChoice() {
+    let playerChoice = prompt('What do you choose - Rock, Paper, or Scissors?', '');
+    playerChoice = playerChoice.toLowerCase();
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
+    return playerChoice;
+}
+
 /* single round of rock, paper, scissors*/
 /* prompt player for selection, take player's selection (case-insensitive) and computer's selection as input
 set conditions to decide who wins
+gives winner a point
 declare whether payer wins or loses and what item beat what */
-function playRound(computerSelection) {
-    let playerSelection = prompt('What do you choose - Rock, Paper, or Scissors?', '');
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+let playerTotal = 0;
+let computerTotal = 0;
+
+function playRound(playerSelection, computerSelection) {
     if(playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors'){
         if(playerSelection === computerSelection) {
-            return `It's a tie! You both chose ${computerSelection}.`;
+            return console.log(`It's a tie! You both chose ${computerSelection}.`);
         } else if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
-            return `You won! ${playerSelection} beats ${computerSelection}.`;
+            playerTotal += 1;
+            return console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
         } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
-            return `You won! ${playerSelection} beats ${computerSelection}.`;
+            playerTotal += 1;
+            return console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
         } else if(playerSelection === 'Paper' && computerSelection === 'Rock') {
-            return `You won! ${playerSelection} beats ${computerSelection}.`;
+            playerTotal += 1;
+            return console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
         } else {
-            return `You lost! ${computerSelection} beats ${playerSelection}.`
+            computerTotal += 1;
+            return console.log(`You lost! ${computerSelection} beats ${playerSelection}.`);
         }
-
     } else {
-        return `Your choice was invalid. Try Again.`
+        return console.log(`Your choice was invalid. Try Again.`);
     }
+    
 }
 
 /* game plays for 5 rounds - keeps score and declares winner at the the end */
-/* add one point to winners overall score per round
-if player enters invalid choice restart round
-declare winner after 5 successful rounds */
+/* reset scores at start of game
+loop through 5 rounds of rock, paper, scissors calling playRound function
+output string declaring overall winner and total player won or lost by */
 
