e715c096fa8106dfd1c81a48e0182ab42e5cc15c
Zan Zirbel <zan.zirbel@gmail.com>
2022-10-20 14:39:29 -0700

    change game function to while loop, repeat until player or computer get 5 points

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ set conditions to decide who wins
 add a point to winners total points
 declare whether payer wins or loses and what item beat what */
 function playRound(computerSelection) {
-    let playerSelection = prompt('What do you choose - Rock, Paper, or Scissors?', '');
+    let playerSelection = prompt('What do you choose - Rock, Paper, or Scissors?', 'Choose one');
  playerSelection = playerSelection.toLowerCase();
  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
     if(playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors'){
@@ -45,23 +45,21 @@ function playRound(computerSelection) {
     
 }
 
-/* game plays for 5 rounds and declares winner at the the end */
+/* game plays until player or computer reach 5 points and declares winner at the the end */
 /* reset scores at start of game
-loop through 5 rounds of rock, paper, scissors calling playRound function
+loop through playRound function
 output string declaring overall winner and total that player won or lost by */
 function game() {
     playerTotal = 0;
     computerTotal = 0;
-    for(let i = 1; i <= 5; i++) {
+    while(playerTotal < 5 && computerTotal < 5) {
     playRound(getComputerChoice());
     console.log(`Your Total: ${playerTotal}`);
     console.log(`Computers Total: ${computerTotal}`);
-    }
+    };
     if(playerTotal > computerTotal) {
         console.log(`Congratulations! You beat the computer by ${playerTotal - computerTotal}.`);
     }else if(playerTotal < computerTotal) {
         console.log(`Better luck next time! You lost to the computer by ${computerTotal - playerTotal}.`);
-    }else{
-        console.log(`Wow it's a draw!`);
     }
 }
